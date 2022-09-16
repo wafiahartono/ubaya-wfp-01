80c0e8e1f60755a9c72116f65660646d44abbaeb
Wafi Hartono <s160419098@student.ubaya.ac.id>
2022-09-16 15:18:04 +0700

    W03

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->insert([
+            'nrp' => '160419084',
+            'name' => 'Amirullah Achmad Nassardhi'
+        ]);
+
+        DB::table('students')->insert([
+            'nrp' => '160419098',
+            'name' => 'Wafi Azmi Hartono'
+        ]);
+
+        DB::table('students')->insert([
+            'nrp' => '160419112',
+            'name' => 'Yudhistira Anggara Jayadinata'
+        ]);
+    }
+}
